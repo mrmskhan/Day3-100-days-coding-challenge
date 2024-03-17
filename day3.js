abab3faa3fbdbd0d279a50b357a7b2dd7024e727
M. SALEEM KHAN <160552274+mrmskhan@users.noreply.github.com>
2024-03-18 03:32:14 +0500

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,20 @@
+// Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//  Enclose your operations in print statements.
+console.log("Addition ".concat(5 + 3));
+console.log("Subtraction ".concat(10 - 2));
+console.log("Multiplication ".concat(2 * 4));
+console.log("Division ".concat(16 / 2));
+// Question 8: You should create four lines that look like this:
+// Addition
+console.log(5 + 3);
+// Subtraction
+console.log(10 - 2);
+// Multiplication
+console.log(2 * 4);
+// division
+console.log(16 / 2);
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that 
+// message.
+var favoriteNumber = 20;
+var massege = "My Favorite Number is ".concat(favoriteNumber);
+console.log(massege);
---
+++ day3.ts
@@ -0,0 +1,27 @@
+// Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//  Enclose your operations in print statements.
+
+
+console.log(`Addition ${5 + 3}`)
+console.log(`Subtraction ${10 - 2}`)
+console.log(`Multiplication ${2 * 4}`)
+console.log(`Division ${16 / 2}`)
+
+
+// Question 8: You should create four lines that look like this:
+// Addition
+console.log(5 + 3)
+// Subtraction
+console.log(10 - 2)
+// Multiplication
+console.log(2 * 4)
+// division
+console.log(16 / 2)
+
+
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that 
+// message.
+
+let favoriteNumber:number= 20
+let massege:string=`My Favorite Number is ${favoriteNumber}`
+console.log(massege)
